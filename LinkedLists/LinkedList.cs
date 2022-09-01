733c493ec267a59f4daf86352871a2ad579e1f8e
tahirbey48 <itkokten@gmail.com>
2022-09-01 21:25:00 +0300

    more methods were added to linkedlist

--- LinkedLists/LinkedList.cs
+++ LinkedLists/LinkedList.cs
@@ -29,7 +29,7 @@ public class LinkedList
         Node tmp = head;
         while (tmp != null)
         {
-            Debug.Log("the item of LinkedList                     " + tmp.value);
+            Debug.Log("the item of LinkedList **********************       " + tmp.value);
             tmp = tmp.next;
         }
     }
@@ -54,8 +54,123 @@ public class LinkedList
     }
 
 
+    public void prepend(int value)
+    {
+        Node newnode = new Node(value);
+        if (head == null)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            newnode.next = head;
+            head = newnode;
+        }
+        length++;
+    }
+
+    public void pop()
+    {
+        if (length == 0)
+            return;
+        else if (length == 1)
+        {
+            head = null;
+            tail = null;
+        }
+        else
+        {
+            Node tmp = head;
+            while (tmp.next != tail)
+            {
+                tmp = tmp.next;
+            }
+            tmp.next = null;
+            tail = tmp;
+        }
+        length--;
+
+    }
+
+    public void pop_first()
+    {
+        if (head == null)
+            return;
+        else if (length == 1)
+        {
+            head = null;
+            tail = null;
+        } else
+        {
+            Node tmp = head;
+            head = tmp.next;
+            tmp.next = null;
+        }
+        length--;
+
+    }
 
 
+    public int get_value(int index)
+    {
+        Node tmp = head;
+        int i = 0;
+        while (i<index)
+        {
+            tmp = tmp.next;
+            i++;
+        }
+        return tmp.value;
+    }
+
+
+    public Node get_node(int index)
+    {
+        Node tmp = head;
+        int i = 0;
+        while (i < index)
+        {
+            tmp = tmp.next;
+            i++;
+        }
+        return tmp;
+    }
+
+
+
+    public void remove(int index)
+    {
+        if (index > length && index <0)
+            return;
+        if (head == null)
+            return;
+        if (index == 0)
+        {
+            this.pop_first();
+        } else if (index == length)
+        {
+            this.pop();
+        } else
+        {
+            Node before = get_node(index);
+            Node tmp = before.next;
+            before.next = tmp.next;
+            tmp.next = null;
+        }
+        length--;
+    }
+
+
+    public void set_value(int index, int value)
+    {
+        if (index > length && index < 0)
+            return;
+        if (length == 0)
+            return;
+        Node tmp = get_node(index);
+        tmp.value = value;
+    }
 
 
 
@@ -63,13 +178,13 @@ public class LinkedList
 
 
 
-[SerializeField]
-class Node
+//[SerializeField]
+public class Node
 {
     public int value;
     public Node next;
 
-    public Node(int i)  //constructor
+    public Node(int i) 
     {
         value = i;
         next = null;
--- RunAlgos.cs
+++ RunAlgos.cs
@@ -10,24 +10,26 @@ public class RunAlgos : MonoBehaviour
 
     void Start()
     {
-        myLinkedList = new LinkedList(1);
+        myLinkedList = new LinkedList(0);
+        myLinkedList.append(1);
         myLinkedList.append(2);
         myLinkedList.append(3);
         myLinkedList.append(4);
+        myLinkedList.append(5);
+        myLinkedList.append(6);
+        myLinkedList.append(7);
+        myLinkedList.append(8);
+        myLinkedList.append(9);
+        myLinkedList.append(10);
+        myLinkedList.append(11);
+        myLinkedList.set_value(5, 500);
+        myLinkedList.remove(6);
+        myLinkedList.pop_first();
+        myLinkedList.pop();
+        myLinkedList.prepend(100);
         Debug.Log("Linkedlist length is    " + myLinkedList.length);
         myLinkedList.print_list();
     }
 
-
-
-
-
-
-
-    // Update is called once per frame
-    //void Update()
-    //{
-        
-
-    //}
 }
+
